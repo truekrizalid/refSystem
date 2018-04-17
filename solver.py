0b5cd22b76cb49111875f507b25e29160bf54cb3
truekrizalid <yangfan.meiling@aliyun.com>
2018-04-17 22:38:47 +0800

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+from Compressor import compressor 
+from RefCycle import refCycle
+import CoolProp.CoolProp as CP
+
+cmpName =  "VTH1113YA"      #raw_input(">>please input compressor model:\n")
+UAk = 20          #raw_input(">>Please input condenser UA value(W/K)")
+SLH = 0.89            #raw_input(">>Suction Line heat exchange effectiveness")
+UAe = 24
+UAcmp = 1.3
+UAcab = 50
+Tamb = 32
+def f1(CompName,CompUA):
+    pass
